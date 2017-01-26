a6b32a18a0a9eb9e7c81e836aa9e89cbe7291f91
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2017-01-26 14:20:45 +0100

    added 2016 full dataset scale factors

---
+++ compareFunctions2016BtoH.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+import ROOT
+import imp
+import CombineHarvester.CombineTools.plotting as plot
+wsptools = imp.load_source('wsptools', 'workspaceTools.py')
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gROOT.SetBatch(ROOT.kTRUE)
+plot.ModTDRStyle()
+
+fin = ROOT.TFile('htt_scalefactors_v16_1.root')
+w = fin.Get('w')
+
+
+def Compare(w, output, fn1, fn2, bins, var, other_vars= {}, line_pos=None):
+    label_vars = []
+    for key, val in other_vars.iteritems():
+        w.var(key).setVal(val)
+        label_vars.append('%s=%g' % (key, val))
+    h1 = w.function(fn1).createHistogram(fn1, w.var(var),
+            ROOT.RooFit.Binning(*bins),
+            ROOT.RooFit.Scaling(False)
+        )
+    h2 = w.function(fn2).createHistogram(fn2, w.var(var),
+            ROOT.RooFit.Binning(*bins),
+            ROOT.RooFit.Scaling(False)
+        )
+    canv = ROOT.TCanvas(output, output)
+    pads = plot.TwoPadSplit(0.30, 0.01, 0.01)
+    pads[0].cd()
+    pads[0].SetGrid(1, 1)
+    plot.Set(h1, LineColor=ROOT.kBlack, LineWidth=2)
+    plot.Set(h1.GetYaxis(), Title='Scale Factor')
+    plot.Set(h2, LineColor=ROOT.kRed, LineWidth=2)
+
+    for i in xrange(1, h1.GetNbinsX()+1):
+        h1.SetBinError(i, 0.)
+    for i in xrange(1, h2.GetNbinsX()+1):
+        h2.SetBinError(i, 0.)
+    h1.Draw('L')
+    h2.Draw('LSAME')
+    ratio = h1.Clone()
+    ratio.Divide(h2)
+
+
+    legend = ROOT.TLegend(0.18, 0.82, 0.6, 0.93, '', 'NBNDC')
+    legend.AddEntry(h1, fn1, 'L')
+    legend.AddEntry(h2, fn2, 'L')
+    legend.Draw()
+    print plot.GetPadYMax(pads[0])
+    plot.FixTopRange(pads[0], 1.5, 0) #plot.GetPadYMax(pads[0]), 0.25)
+    plot.DrawTitle(pads[0], ','.join(label_vars), 1)
+
+    line = ROOT.TLine()
+    plot.Set(line, LineColor=12, LineStyle=4, LineWidth=2)
+    if line_pos is not None:
+        plot.DrawVerticalLine(pads[0], line, line_pos)
+
+    pads[1].cd()
+    pads[1].SetGrid(1, 1)
+    ratio.Draw('L')
+    plot.SetupTwoPadSplitAsRatio(
+        pads, plot.GetAxisHist(
+            pads[0]), plot.GetAxisHist(pads[1]), 'Ratio', True, 0.91, 1.09)
+    if line_pos is not None:
+        plot.DrawVerticalLine(pads[1], line, line_pos)
+    canv.Print('.pdf')
+    canv.Print('.png')
+
+w.var('m_eta').setVal(1.3)
+Compare(w, 'compare_kit_desy_m_trg24_ratio_low_pt_eta_0p0', 'm_trg24_ratio', 'm_trgIsoMu24_desy_ratio', bins=[200, 0, 50], var='m_pt', other_vars={'m_eta':0.0}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24_ratio_low_pt_eta_0p9', 'm_trg24_ratio', 'm_trgIsoMu24_desy_ratio', bins=[200, 0, 50], var='m_pt', other_vars={'m_eta':0.9}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24_ratio_low_pt_eta_1p2', 'm_trg24_ratio', 'm_trgIsoMu24_desy_ratio', bins=[200, 0, 50], var='m_pt', other_vars={'m_eta':1.2}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24_ratio_low_pt_eta_2p1', 'm_trg24_ratio', 'm_trgIsoMu24_desy_ratio', bins=[200, 0, 50], var='m_pt', other_vars={'m_eta':2.1}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24_ratio_eta_0p0', 'm_trg24_ratio', 'm_trgIsoMu24_desy_ratio', bins=[1000, 0, 1000], var='m_pt', other_vars={'m_eta':0.0}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24_ratio_eta_0p9', 'm_trg24_ratio', 'm_trgIsoMu24_desy_ratio', bins=[1000, 0, 1000], var='m_pt', other_vars={'m_eta':0.9}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24_ratio_eta_1p2', 'm_trg24_ratio', 'm_trgIsoMu24_desy_ratio', bins=[1000, 0, 1000], var='m_pt', other_vars={'m_eta':1.2}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24_ratio_eta_2p1', 'm_trg24_ratio', 'm_trgIsoMu24_desy_ratio', bins=[1000, 0, 1000], var='m_pt', other_vars={'m_eta':2.1}, line_pos=23.)
+
+Compare(w, 'compare_kit_desy_m_trg24OR_ratio_low_pt_eta_0p0', 'm_trg24OR_ratio', 'm_trgIsoMu24orTkIsoMu24_desy_ratio', bins=[200, 0, 50], var='m_pt', other_vars={'m_eta':0.0}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24OR_ratio_low_pt_eta_0p9', 'm_trg24OR_ratio', 'm_trgIsoMu24orTkIsoMu24_desy_ratio', bins=[200, 0, 50], var='m_pt', other_vars={'m_eta':0.9}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24OR_ratio_low_pt_eta_1p2', 'm_trg24OR_ratio', 'm_trgIsoMu24orTkIsoMu24_desy_ratio', bins=[200, 0, 50], var='m_pt', other_vars={'m_eta':1.2}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24OR_ratio_low_pt_eta_2p1', 'm_trg24OR_ratio', 'm_trgIsoMu24orTkIsoMu24_desy_ratio', bins=[200, 0, 50], var='m_pt', other_vars={'m_eta':2.1}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24OR_ratio_eta_0p0', 'm_trg24OR_ratio', 'm_trgIsoMu24orTkIsoMu24_desy_ratio', bins=[1000, 0, 1000], var='m_pt', other_vars={'m_eta':0.0}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24OR_ratio_eta_0p9', 'm_trg24OR_ratio', 'm_trgIsoMu24orTkIsoMu24_desy_ratio', bins=[1000, 0, 1000], var='m_pt', other_vars={'m_eta':0.9}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24OR_ratio_eta_1p2', 'm_trg24OR_ratio', 'm_trgIsoMu24orTkIsoMu24_desy_ratio', bins=[1000, 0, 1000], var='m_pt', other_vars={'m_eta':1.2}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_trg24OR_ratio_eta_2p1', 'm_trg24OR_ratio', 'm_trgIsoMu24orTkIsoMu24_desy_ratio', bins=[1000, 0, 1000], var='m_pt', other_vars={'m_eta':2.1}, line_pos=23.)
+
+
+Compare(w, 'compare_kit_desy_m_idiso_ratio_eta_0p0', 'm_idiso_ratio', 'm_idiso0p15_desy_ratio', bins=[200, 0, 200], var='m_pt', other_vars={'m_eta':0.0}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_idiso_ratio_eta_0p9', 'm_idiso_ratio', 'm_idiso0p15_desy_ratio', bins=[200, 0, 200], var='m_pt', other_vars={'m_eta':0.9}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_idiso_ratio_eta_1p2', 'm_idiso_ratio', 'm_idiso0p15_desy_ratio', bins=[200, 0, 200], var='m_pt', other_vars={'m_eta':1.2}, line_pos=23.)
+Compare(w, 'compare_kit_desy_m_idiso_ratio_eta_2p1', 'm_idiso_ratio', 'm_idiso0p15_desy_ratio', bins=[200, 0, 200], var='m_pt', other_vars={'m_eta':2.1}, line_pos=23.)
+
+Compare(w, 'compare_kit_desy_e_trg_ratio_low_pt_eta_0p0', 'e_trg_ratio', 'e_trgEle25eta2p1WPTight_desy_ratio', bins=[200, 0, 50], var='e_pt', other_vars={'e_eta':0.0}, line_pos=26.)
+Compare(w, 'compare_kit_desy_e_trg_ratio_low_pt_eta_2p0', 'e_trg_ratio', 'e_trgEle25eta2p1WPTight_desy_ratio', bins=[200, 0, 50], var='e_pt', other_vars={'e_eta':2.0}, line_pos=26.)
+Compare(w, 'compare_kit_desy_e_trg_ratio_low_pt_eta_2p4', 'e_trg_ratio', 'e_trgEle25eta2p1WPTight_desy_ratio', bins=[200, 0, 50], var='e_pt', other_vars={'e_eta':2.4}, line_pos=26.)
+Compare(w, 'compare_kit_desy_e_trg_ratio_eta_0p0', 'e_trg_ratio', 'e_trgEle25eta2p1WPTight_desy_ratio', bins=[1000, 0, 1000], var='e_pt', other_vars={'e_eta':0.0}, line_pos=26.)
+Compare(w, 'compare_kit_desy_e_trg_ratio_eta_2p0', 'e_trg_ratio', 'e_trgEle25eta2p1WPTight_desy_ratio', bins=[1000, 0, 1000], var='e_pt', other_vars={'e_eta':2.0}, line_pos=26.)
+Compare(w, 'compare_kit_desy_e_trg_ratio_eta_2p4', 'e_trg_ratio', 'e_trgEle25eta2p1WPTight_desy_ratio', bins=[1000, 0, 1000], var='e_pt', other_vars={'e_eta':2.4}, line_pos=26.)
+Compare(w, 'compare_kit_desy_e_idiso_ratio_eta_0p0', 'e_idiso_ratio', 'e_idiso0p10_desy_ratio', bins=[200, 0, 200], var='e_pt', other_vars={'e_eta':0.0}, line_pos=26.)
+Compare(w, 'compare_kit_desy_e_idiso_ratio_eta_2p0', 'e_idiso_ratio', 'e_idiso0p10_desy_ratio', bins=[200, 0, 200], var='e_pt', other_vars={'e_eta':2.0}, line_pos=26.)
+Compare(w, 'compare_kit_desy_e_idiso_ratio_eta_2p4', 'e_idiso_ratio', 'e_idiso0p10_desy_ratio', bins=[200, 0, 200], var='e_pt', other_vars={'e_eta':2.4}, line_pos=26.)
+# w.writeToFile('htt_scalefactors_v2.root')
+# w.Delete()
---
+++ makeCorrectionsWorkspace_16.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+import ROOT
+import imp
+import json
+from array import array
+wsptools = imp.load_source('wsptools', 'workspaceTools.py')
+
+
+def GetFromTFile(str):
+    f = ROOT.TFile(str.split(':')[0])
+    obj = f.Get(str.split(':')[1]).Clone()
+    f.Close()
+    return obj
+
+# Boilerplate
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gROOT.SetBatch(ROOT.kTRUE)
+ROOT.RooWorkspace.imp = getattr(ROOT.RooWorkspace, 'import')
+ROOT.TH1.AddDirectory(0)
+ROOT.gROOT.LoadMacro("CrystalBallEfficiency.cxx+")
+
+w = ROOT.RooWorkspace('w')
+
+
+### KIT electron/muon tag and probe results
+loc = 'inputs/KIT/v16_1'
+
+histsToWrap = [
+    (loc+'/ZmmTP_Data_Fits_ID_pt_eta_bins.root:ID_pt_eta_bins',                    'm_id_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_ID_pt_eta_bins.root:ID_pt_eta_bins',              'm_id_mc'),
+    (loc+'/ZmmTP_Data_Fits_Iso_pt_eta_bins.root:Iso_pt_eta_bins',                        'm_iso_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_Iso_pt_eta_bins.root:Iso_pt_eta_bins',                  'm_iso_mc'),
+    (loc+'/ZmmTP_Data_Fits_AIso1_pt_eta_bins.root:AIso1_pt_eta_bins',                    'm_aiso1_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_AIso1_pt_eta_bins.root:AIso1_pt_eta_bins',              'm_aiso1_mc'),
+    (loc+'/ZmmTP_Data_Fits_AIso2_pt_eta_bins.root:AIso2_pt_eta_bins',                    'm_aiso2_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_AIso2_pt_eta_bins.root:AIso2_pt_eta_bins',              'm_aiso2_mc'),
+    (loc+'/ZmmTP_Data_Fits_Trg_Iso_pt_eta_bins.root:Trg_Iso_pt_eta_bins',                'm_trg_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_Trg_Iso_pt_eta_bins.root:Trg_Iso_pt_eta_bins',                'm_trg_mc'),
+    (loc+'/ZmmTP_Data_Fits_Trg_AIso1_pt_bins_inc_eta.root:Trg_AIso1_pt_bins_inc_eta',    'm_trg_aiso1_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_Trg_AIso1_pt_bins_inc_eta.root:Trg_AIso1_pt_bins_inc_eta',    'm_trg_aiso1_mc'),
+    (loc+'/ZmmTP_Data_Fits_Trg_AIso2_pt_bins_inc_eta.root:Trg_AIso2_pt_bins_inc_eta',    'm_trg_aiso2_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_Trg_AIso2_pt_bins_inc_eta.root:Trg_AIso2_pt_bins_inc_eta',    'm_trg_aiso2_mc'),
+    (loc+'/ZmmTP_Data_Fits_TrgOR_Iso_pt_eta_bins.root:TrgOR_Iso_pt_eta_bins',              'm_trgOR_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_TrgOR_Iso_pt_eta_bins.root:TrgOR_Iso_pt_eta_bins',              'm_trgOR_mc'),
+    (loc+'/ZmmTP_Data_Fits_TrgOR_AIso1_pt_bins_inc_eta.root:TrgOR_AIso1_pt_bins_inc_eta',  'm_trgOR_aiso1_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_TrgOR_AIso1_pt_bins_inc_eta.root:TrgOR_AIso1_pt_bins_inc_eta',  'm_trgOR_aiso1_mc'),
+    (loc+'/ZmmTP_Data_Fits_TrgOR_AIso2_pt_bins_inc_eta.root:TrgOR_AIso2_pt_bins_inc_eta',  'm_trgOR_aiso2_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_TrgOR_AIso2_pt_bins_inc_eta.root:TrgOR_AIso2_pt_bins_inc_eta',  'm_trgOR_aiso2_mc'),
+    (loc+'/ZmmTP_Data_Fits_TrgOR3_Iso_pt_eta_bins.root:TrgOR3_Iso_pt_eta_bins',              'm_trgOR3_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_TrgOR3_Iso_pt_eta_bins.root:TrgOR3_Iso_pt_eta_bins',              'm_trgOR3_mc'),
+    (loc+'/ZmmTP_Data_Fits_Trg24_Iso_pt_eta_bins.root:Trg24_Iso_pt_eta_bins',                'm_trg24_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_Trg24_Iso_pt_eta_bins.root:Trg24_Iso_pt_eta_bins',                'm_trg24_mc'),
+    (loc+'/ZmmTP_Data_Fits_Trg24OR_Iso_pt_eta_bins.root:Trg24OR_Iso_pt_eta_bins',                'm_trg24OR_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_Trg24OR_Iso_pt_eta_bins.root:Trg24OR_Iso_pt_eta_bins',                'm_trg24OR_mc'),
+    (loc+'/ZmmTP_Data_Fits_Trg24OR3_Iso_pt_eta_bins.root:Trg24OR3_Iso_pt_eta_bins',                'm_trg24OR3_data'),
+    (loc+'/ZmmTP_DYJetsToLL_Fits_Trg24OR3_Iso_pt_eta_bins.root:Trg24OR3_Iso_pt_eta_bins',                'm_trg24OR3_mc')
+]
+
+for task in histsToWrap:
+    wsptools.SafeWrapHist(w, ['m_pt', 'expr::m_abs_eta("TMath::Abs(@0)",m_eta[0])'],
+                          GetFromTFile(task[0]), name=task[1])
+
+
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15, 0.25, 0.50],
+                                   'm_iso_binned_data', ['m_iso_data', 'm_aiso1_data', 'm_aiso2_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15, 0.25, 0.50],
+                                   'm_iso_binned_mc', ['m_iso_mc', 'm_aiso1_mc', 'm_aiso2_mc'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15, 0.25, 0.50],
+                                   'm_trg_binned_data', ['m_trg_data', 'm_trg_aiso1_data', 'm_trg_aiso2_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15, 0.25, 0.50],
+                                   'm_trg_binned_mc', ['m_trg_mc', 'm_trg_aiso1_mc', 'm_trg_aiso2_mc'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15, 0.25, 0.50],
+                                   'm_trgOR_binned_data', ['m_trgOR_data', 'm_trgOR_aiso1_data', 'm_trgOR_aiso2_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15, 0.25, 0.50],
+                                   'm_trgOR_binned_mc', ['m_trgOR_mc', 'm_trgOR_aiso1_mc', 'm_trgOR_aiso2_mc'])
+
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15],
+                                   'm_trgOR3_binned_data', ['m_trgOR3_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15],
+                                   'm_trgOR3_binned_mc', ['m_trgOR3_mc'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15],
+                                   'm_trg24_binned_data', ['m_trg24_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15],
+                                   'm_trg24_binned_mc', ['m_trg24_mc'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15],
+                                   'm_trg24OR_binned_data', ['m_trg24OR_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15],
+                                   'm_trg24OR_binned_mc', ['m_trg24OR_mc'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15],
+                                   'm_trg24OR3_binned_data', ['m_trg24OR3_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'm_iso', [0., 0.15],
+                                   'm_trg24OR3_binned_mc', ['m_trg24OR3_mc'])
+
+
+for t in ['id', 'iso', 'aiso1', 'aiso2', 'iso_binned', 'trg', 'trg_aiso1', 'trg_aiso2', 'trg_binned', 'trgOR', 'trgOR_aiso1', 'trgOR_aiso2', 'trgOR_binned', 'trgOR3', 'trgOR3_binned', 'trg24', 'trg24_binned', 'trg24OR', 'trg24OR_binned', 'trg24OR3', 'trg24OR3_binned']:
+    w.factory('expr::m_%s_ratio("@0/@1", m_%s_data, m_%s_mc)' % (t, t, t))
+
+for t in ['data', 'mc', 'ratio']:
+    w.factory('expr::m_idiso_%s("@0*@1", m_id_%s, m_iso_%s)' % (t, t, t))
+
+loc = 'inputs/KIT/v16_1'
+
+histsToWrap = [
+    (loc+'/ZeeTP_Data_Fits_ID_pt_eta_bins.root:ID_pt_eta_bins',                          'e_id_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_ID_pt_eta_bins.root:ID_pt_eta_bins',                    'e_id_mc'),
+    (loc+'/ZeeTP_Data_Fits_Iso_pt_eta_bins.root:Iso_pt_eta_bins',                        'e_iso_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Iso_pt_eta_bins.root:Iso_pt_eta_bins',                  'e_iso_mc'),
+    (loc+'/ZeeTP_Data_Fits_AIso1_pt_eta_bins.root:AIso1_pt_eta_bins',                    'e_aiso1_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_AIso1_pt_eta_bins.root:AIso1_pt_eta_bins',              'e_aiso1_mc'),
+    (loc+'/ZeeTP_Data_Fits_AIso2_pt_eta_bins.root:AIso2_pt_eta_bins',                    'e_aiso2_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_AIso2_pt_eta_bins.root:AIso2_pt_eta_bins',              'e_aiso2_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg_Iso_pt_eta_bins.root:Trg_Iso_pt_eta_bins',                'e_trg_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg_Iso_pt_eta_bins.root:Trg_Iso_pt_eta_bins',                'e_trg_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg_AIso1_pt_bins_inc_eta.root:Trg_AIso1_pt_bins_inc_eta',    'e_trg_aiso1_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg_AIso1_pt_bins_inc_eta.root:Trg_AIso1_pt_bins_inc_eta',    'e_trg_aiso1_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg_AIso2_pt_bins_inc_eta.root:Trg_AIso2_pt_bins_inc_eta',    'e_trg_aiso2_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg_AIso2_pt_bins_inc_eta.root:Trg_AIso2_pt_bins_inc_eta',    'e_trg_aiso2_mc'),
+    (loc+'/ZeeTP_Data_Fits_TrgOR_Iso_pt_eta_bins.root:TrgOR_Iso_pt_eta_bins',                'e_trgOR_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_TrgOR_Iso_pt_eta_bins.root:TrgOR_Iso_pt_eta_bins',                'e_trgOR_mc'),
+    (loc+'/ZeeTP_Data_Fits_TrgOR_AIso1_pt_bins_inc_eta.root:TrgOR_AIso1_pt_bins_inc_eta',    'e_trgOR_aiso1_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_TrgOR_AIso1_pt_bins_inc_eta.root:TrgOR_AIso1_pt_bins_inc_eta',    'e_trgOR_aiso1_mc'),
+    (loc+'/ZeeTP_Data_Fits_TrgOR_AIso2_pt_bins_inc_eta.root:TrgOR_AIso2_pt_bins_inc_eta',    'e_trgOR_aiso2_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_TrgOR_AIso2_pt_bins_inc_eta.root:TrgOR_AIso2_pt_bins_inc_eta',    'e_trgOR_aiso2_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg27_Iso_pt_eta_bins.root:Trg27_Iso_pt_eta_bins',                'e_trg27_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg27_Iso_pt_eta_bins.root:Trg27_Iso_pt_eta_bins',                'e_trg27_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg27_AIso1_pt_bins_inc_eta.root:Trg27_AIso1_pt_bins_inc_eta',    'e_trg27_aiso1_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg27_AIso1_pt_bins_inc_eta.root:Trg27_AIso1_pt_bins_inc_eta',    'e_trg27_aiso1_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg27_AIso2_pt_bins_inc_eta.root:Trg27_AIso2_pt_bins_inc_eta',    'e_trg27_aiso2_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg27_AIso2_pt_bins_inc_eta.root:Trg27_AIso2_pt_bins_inc_eta',    'e_trg27_aiso2_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg27OR_Iso_pt_eta_bins.root:Trg27OR_Iso_pt_eta_bins',                'e_trg27OR_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg27OR_Iso_pt_eta_bins.root:Trg27OR_Iso_pt_eta_bins',                'e_trg27OR_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg27OR_AIso1_pt_bins_inc_eta.root:Trg27OR_AIso1_pt_bins_inc_eta',    'e_trg27OR_aiso1_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg27OR_AIso1_pt_bins_inc_eta.root:Trg27OR_AIso1_pt_bins_inc_eta',    'e_trg27OR_aiso1_mc'),
+    (loc+'/ZeeTP_Data_Fits_Trg27OR_AIso2_pt_bins_inc_eta.root:Trg27OR_AIso2_pt_bins_inc_eta',    'e_trg27OR_aiso2_data'),
+    (loc+'/ZeeTP_DYJetsToLL_Fits_Trg27OR_AIso2_pt_bins_inc_eta.root:Trg27OR_AIso2_pt_bins_inc_eta',    'e_trg27OR_aiso2_mc')
+]
+
+for task in histsToWrap:
+    wsptools.SafeWrapHist(w, ['e_pt', 'expr::e_abs_eta("TMath::Abs(@0)",e_eta[0])'],
+                          GetFromTFile(task[0]), name=task[1])
+
+
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_iso_binned_data', ['e_iso_data', 'e_aiso1_data', 'e_aiso2_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_iso_binned_mc', ['e_iso_mc', 'e_aiso1_mc', 'e_aiso2_mc'])
+
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_trg_binned_data', ['e_trg_data', 'e_trg_aiso1_data', 'e_trg_aiso2_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_trg_binned_mc', ['e_trg_mc', 'e_trg_aiso1_mc', 'e_trg_aiso2_mc'])
+
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_trgOR_binned_data', ['e_trgOR_data', 'e_trgOR_aiso1_data', 'e_trgOR_aiso2_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_trgOR_binned_mc', ['e_trgOR_mc', 'e_trgOR_aiso1_mc', 'e_trgOR_aiso2_mc'])
+
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_trg27_binned_data', ['e_trg27_data', 'e_trg27_aiso1_data', 'e_trg27_aiso2_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_trg27_binned_mc', ['e_trg27_mc', 'e_trg27_aiso1_mc', 'e_trg27_aiso2_mc'])
+
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_trg27OR_binned_data', ['e_trg27OR_data', 'e_trg27OR_aiso1_data', 'e_trg27OR_aiso2_data'])
+wsptools.MakeBinnedCategoryFuncMap(w, 'e_iso', [0., 0.10, 0.20, 0.50],
+                                   'e_trg27OR_binned_mc', ['e_trg27OR_mc', 'e_trg27OR_aiso1_mc', 'e_trg27OR_aiso2_mc'])
+
+
+
+for t in ['id', 'iso', 'aiso1', 'aiso2', 'iso_binned', 'trg', 'trg_aiso1', 'trg_aiso2', 'trg_binned', 'trgOR', 'trgOR_aiso1', 'trgOR_aiso2', 'trgOR_binned', 'trg27', 'trg27_aiso1', 'trg27_aiso2', 'trg27_binned', 'trg27OR', 'trg27OR_aiso1', 'trg27OR_aiso2', 'trg27OR_binned']:
+    w.factory('expr::e_%s_ratio("@0/@1", e_%s_data, e_%s_mc)' % (t, t, t))
+
+for t in ['data', 'mc', 'ratio']:
+    w.factory('expr::e_idiso_%s("@0*@1", e_id_%s, e_iso_%s)' % (t, t, t))
+
+### Muon tracking efficiency scale factor from the muon POG
+loc = 'inputs/MuonPOG'
+
+muon_trk_eff_hist = wsptools.TGraphAsymmErrorsToTH1D(GetFromTFile(loc+'/ratios.root:ratio_eta'))
+wsptools.SafeWrapHist(w, ['m_eta'], muon_trk_eff_hist, name='m_trk_ratio')
+
+### Electron tracking efficiency scale factor from the egamma POG
+loc = 'inputs/EGammaPOG'
+
+electron_trk_eff_hist = GetFromTFile(loc+'/egammaEffi.txt_SF2D.root:EGamma_SF2D')
+wsptools.SafeWrapHist(w, ['e_eta','e_pt'], electron_trk_eff_hist, name='e_trk_ratio')
+
+
+### DESY electron/muon tag and probe results
+loc = 'inputs/LeptonEfficiencies'
+
+desyHistsToWrap = [
+    (loc+'/Muon/Run2016BtoH/Muon_IdIso_IsoLt0p15_2016BtoH_eff.root',            'MC',   'm_idiso0p15_desy_mc'),
+    (loc+'/Muon/Run2016BtoH/Muon_IdIso_IsoLt0p15_2016BtoH_eff.root',            'Data', 'm_idiso0p15_desy_data'),
+    (loc+'/Muon/Run2016BtoH/Muon_IdIso_IsoLt0p2_2016BtoH_eff.root',            'MC',   'm_idiso0p20_desy_mc'),
+    (loc+'/Muon/Run2016BtoH/Muon_IdIso_IsoLt0p2_2016BtoH_eff.root',            'Data', 'm_idiso0p20_desy_data'),
+    (loc+'/Muon/Run2016BtoH/Muon_IsoMu24_2016BtoH_eff.root',              'MC', 'm_trgIsoMu24_desy_mc'),
+    (loc+'/Muon/Run2016BtoH/Muon_IsoMu24_2016BtoH_eff.root',              'Data', 'm_trgIsoMu24_desy_data'),
+    (loc+'/Muon/Run2016BtoH/Muon_IsoMu24_OR_TkIsoMu24_2016BtoH_eff.root', 'MC', 'm_trgIsoMu24orTkIsoMu24_desy_mc'),
+    (loc+'/Muon/Run2016BtoH/Muon_IsoMu24_OR_TkIsoMu24_2016BtoH_eff.root', 'Data', 'm_trgIsoMu24orTkIsoMu24_desy_data'),
+    (loc+'/Muon/Run2016BtoH/Muon_Mu8leg_2016BtoH_eff.root',               'Data', 'm_trgMu8leg_desy_data'),
+    (loc+'/Muon/Run2016BtoH/Muon_Mu23leg_2016BtoH_eff.root',              'Data', 'm_trgMu23leg_desy_data')
+]
+
+for task in desyHistsToWrap:
+    wsptools.SafeWrapHist(w, ['m_pt', 'expr::m_abs_eta("TMath::Abs(@0)",m_eta[0])'],
+                          wsptools.ProcessDESYLeptonSFs(task[0], task[1], task[2]), name=task[2])
+for t in ['idiso0p15_desy', 'idiso0p20_desy', 'trgIsoMu24_desy', 'trgIsoMu24orTkIsoMu24_desy']:
+    w.factory('expr::m_%s_ratio("@0/@1", m_%s_data, m_%s_mc)' % (t, t, t))
+
+desyHistsToWrap = [
+    (loc+'/Electron/Run2016BtoH/Electron_IdIso_IsoLt0p1_2016BtoH_eff.root',          'MC',   'e_idiso0p10_desy_mc'),
+    (loc+'/Electron/Run2016BtoH/Electron_IdIso_IsoLt0p1_2016BtoH_eff.root',          'Data', 'e_idiso0p10_desy_data'),
+    (loc+'/Electron/Run2016BtoH/Electron_IdIso_IsoLt0p15_2016BtoH_eff.root',          'MC',   'e_idiso0p15_desy_mc'),
+    (loc+'/Electron/Run2016BtoH/Electron_IdIso_IsoLt0p15_2016BtoH_eff.root',          'Data', 'e_idiso0p15_desy_data'),
+    (loc+'/Electron/Run2016BtoH/Electron_Ele25_eta2p1_WPTight_2016BtoH_eff.root', 'MC', 'e_trgEle25eta2p1WPTight_desy_mc'),
+    (loc+'/Electron/Run2016BtoH/Electron_Ele25_eta2p1_WPTight_2016BtoH_eff.root', 'Data', 'e_trgEle25eta2p1WPTight_desy_data'),
+    (loc+'/Electron/Run2016BtoH/Electron_Ele12leg_2016BtoH_eff.root',           'Data', 'e_trgEle12leg_desy_data'),
+    (loc+'/Electron/Run2016BtoH/Electron_Ele23leg_2016BtoH_eff.root',           'Data', 'e_trgEle23leg_desy_data')
+]
+
+for task in desyHistsToWrap:
+    wsptools.SafeWrapHist(w, ['e_pt', 'expr::e_abs_eta("TMath::Abs(@0)",e_eta[0])'],
+                          wsptools.ProcessDESYLeptonSFs(task[0], task[1], task[2]), name=task[2])
+
+for t in ['idiso0p10_desy', 'idiso0p15_desy', 'trgEle25eta2p1WPTight_desy']:
+    w.factory('expr::e_%s_ratio("@0/@1", e_%s_data, e_%s_mc)' % (t, t, t))
+
+### KIT tau ID scale factors
+loc = 'inputs/KIT/tau_id_sfs_2016.root:'
+histsToWrap = [
+    (loc + 'mva_m_dm0_pt30', 't_iso_mva_m_dm0_pt30_sf'),
+    (loc + 'mva_m_dm1_pt30', 't_iso_mva_m_dm1_pt30_sf'),
+    (loc + 'mva_m_dm10_pt30', 't_iso_mva_m_dm10_pt30_sf'),
+    (loc + 'mva_t_dm0_pt40_eta2p1', 't_iso_mva_t_dm0_pt40_eta2p1_sf'),
+    (loc + 'mva_t_dm1_pt40_eta2p1', 't_iso_mva_t_dm1_pt40_eta2p1_sf'),
+    (loc + 'mva_t_dm10_pt40_eta2p1', 't_iso_mva_t_dm10_pt40_eta2p1_sf'),
+]
+for task in histsToWrap:
+    wsptools.SafeWrapHist(w, ['t_pt', 'expr::t_abs_eta("TMath::Abs(@0)",t_eta[0])'],
+                          GetFromTFile(task[0]), name=task[1])
+
+wsptools.MakeBinnedCategoryFuncMap(w, 't_dm', [-0.5, 0.5, 9.5, 10.5],
+                                   't_iso_mva_m_pt30_sf', ['t_iso_mva_m_dm0_pt30_sf', 't_iso_mva_m_dm1_pt30_sf', 't_iso_mva_m_dm10_pt30_sf'])
+
+wsptools.MakeBinnedCategoryFuncMap(w, 't_dm', [-0.5, 0.5, 9.5, 10.5],
+                                   't_iso_mva_t_pt40_eta2p1_sf', ['t_iso_mva_t_dm0_pt40_eta2p1_sf', 't_iso_mva_t_dm1_pt40_eta2p1_sf', 't_iso_mva_t_dm10_pt40_eta2p1_sf'])
+
+
+### Hadronic tau trigger efficiencies
+loc = 'inputs/triggerSF/di-tau'
+
+with open(loc+'/real_taus_cumulative.json') as jsonfile:
+    pars = json.load(jsonfile)
+    for wp in pars:
+        x = pars[wp]
+        w.factory('CrystalBallEfficiency::t_trg%s_data(t_pt[0],%g,%g,%g,%g,%g)' % (
+                wp, x['m_{0}'], x['sigma'], x['alpha'], x['n'], x['norm']
+            ))
+
+with open(loc+'/same_sign_cumulative.json') as jsonfile:
+    pars = json.load(jsonfile)
+    for wp in pars:
+        x = pars[wp]
+        w.factory('CrystalBallEfficiency::t_trg%sSS_data(t_pt[0],%g,%g,%g,%g,%g)' % (
+                wp, x['m_{0}'], x['sigma'], x['alpha'], x['n'], x['norm']
+            ))
+
+### LO DYJetsToLL Z mass vs pT correction
+wsptools.SafeWrapHist(w, ['z_gen_mass', 'z_gen_pt'],
+                      GetFromTFile('inputs/zpt_weights_2016.root:zptmass_histo'), name='zpt_weight')
+
+w.importClassCode('CrystalBallEfficiency')
+
+w.Print()
+w.writeToFile('htt_scalefactors_v16_1.root')
+w.Delete()
