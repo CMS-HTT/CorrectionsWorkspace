75f18b73e02be1d11371141594fb5cd6b11f24c3
Andrew Gilbert <andrew.gilbert@cern.ch>
2017-02-20 22:18:32 +0100

    adding convenience functions for eta binning and dm binning

--- makeCorrectionsWorkspace_minimal_sm.py
+++ makeCorrectionsWorkspace_minimal_sm.py
@@ -40,97 +40,108 @@ w = ROOT.RooWorkspace('w')
 #         w.factory('CrystalBallEfficiency::t_trg%sSS_data(t_pt[0],%g,%g,%g,%g,%g)' % (
 #                 wp, x['m_{0}'], x['sigma'], x['alpha'], x['n'], x['norm']
 #             ))
-
+interpOrder = 1
 
 tau_mt_file = ROOT.TFile('inputs/TauTriggerMoriond17/trigger_sf_mt.root')
-for obj in [
-            'genuine_barrel_NoIso',
-            'genuine_barrel_VLooseIso',
-            'genuine_barrel_LooseIso',
-            'genuine_barrel_MediumIso',
-            'genuine_barrel_TightIso',
-            'genuine_barrel_VTightIso',
-            'genuine_barrel_VVTightIso',
-            'genuine_endcap_NoIso',
-            'genuine_endcap_VLooseIso',
-            'genuine_endcap_LooseIso',
-            'genuine_endcap_MediumIso',
-            'genuine_endcap_TightIso',
-            'genuine_endcap_VTightIso',
-            'genuine_endcap_VVTightIso',
-            'fake_barrel_NoIso',
-            'fake_barrel_VLooseIso',
-            'fake_barrel_LooseIso',
-            'fake_barrel_MediumIso',
-            'fake_barrel_TightIso',
-            'fake_barrel_VTightIso',
-            'fake_barrel_VVTightIso',
-            'fake_endcap_NoIso',
-            'fake_endcap_VLooseIso',
-            'fake_endcap_LooseIso',
-            'fake_endcap_MediumIso',
-            'fake_endcap_TightIso',
-            'fake_endcap_VTightIso',
-            'fake_endcap_VVTightIso'
-           ]:
-    wsptools.SafeWrapHist(w, ['t_pt'],
-                          wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_mt_file.Get('data_%s' % obj)), name='t_%s_mt_data' % obj)
-    w.function('t_%s_mt_data' % obj).setInterpolationOrder(1)
-    wsptools.SafeWrapHist(w, ['t_pt'],
-                          wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_mt_file.Get('mc_%s' % obj)), name='t_%s_mt_mc' % obj)
-    w.function('t_%s_mt_mc' % obj).setInterpolationOrder(1)
-    w.factory('expr::t_%s_mt_ratio("@0/@1", t_%s_mt_data, t_%s_mt_mc)' % (obj, obj, obj))
+for tautype in ['genuine', 'fake']:
+    for iso in ['NoIso',
+                'VLooseIso',
+                'LooseIso',
+                'MediumIso',
+                'TightIso',
+                'VTightIso',
+                'VVTightIso']:
+        for region in ['barrel', 'endcap']:
+            label = '%s_%s_%s' % (tautype, region, iso)
+            wsptools.SafeWrapHist(w, ['t_pt'], wsptools.TGraphAsymmErrorsToTH1DForTaus(
+                tau_mt_file.Get('data_%s' % label)), name='t_%s_mt_data' % label)
+            wsptools.SafeWrapHist(w, ['t_pt'], wsptools.TGraphAsymmErrorsToTH1DForTaus(
+                tau_mt_file.Get('mc_%s' % label)), name='t_%s_mt_mc' % label)
+            w.function('t_%s_mt_data' % label).setInterpolationOrder(interpOrder)
+            w.function('t_%s_mt_mc' % label).setInterpolationOrder(interpOrder)
+            w.factory('expr::t_%s_mt_ratio("@0/@1", t_%s_mt_data, t_%s_mt_mc)' % (label, label, label))
+        fn = w.factory('expr::t_%s_%s_mt_ratio("TMath::Abs(@0) < 1.5 ? @1 : @2", t_eta[0], t_%s_barrel_%s_mt_ratio, t_%s_endcap_%s_mt_ratio)' %
+            (tautype, iso, tautype, iso, tautype, iso))
+
 tau_mt_file.Close()
 
 
 tau_et_file = ROOT.TFile('inputs/TauTriggerMoriond17/trigger_sf_et.root')
-for obj in [
-            'genuine_barrel_NoIso',
-            'genuine_barrel_VLooseIso',
-            'genuine_barrel_LooseIso',
-            'genuine_barrel_MediumIso',
-            'genuine_barrel_TightIso',
-            'genuine_barrel_VTightIso',
-            'genuine_barrel_VVTightIso',
-            'genuine_endcap_NoIso',
-            'genuine_endcap_VLooseIso',
-            'genuine_endcap_LooseIso',
-            'genuine_endcap_MediumIso',
-            'genuine_endcap_TightIso',
-            'genuine_endcap_VTightIso',
-            'genuine_endcap_VVTightIso',
-            'fake_barrel_NoIso',
-            'fake_barrel_VLooseIso',
-            'fake_barrel_LooseIso',
-            'fake_barrel_MediumIso',
-            'fake_barrel_TightIso',
-            'fake_barrel_VTightIso',
-            'fake_barrel_VVTightIso',
-            'fake_endcap_NoIso',
-            'fake_endcap_VLooseIso',
-            'fake_endcap_LooseIso',
-            'fake_endcap_MediumIso',
-            'fake_endcap_TightIso',
-            'fake_endcap_VTightIso',
-            'fake_endcap_VVTightIso'
-           ]:
-    wsptools.SafeWrapHist(w, ['t_pt'],
-                          wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_et_file.Get('data_%s_dm0' % obj)), name='t_%s_dm0_et_data' % obj)
-    w.function('t_%s_dm0_et_data' % obj).setInterpolationOrder(1)
-    wsptools.SafeWrapHist(w, ['t_pt'],
-                          wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_et_file.Get('data_%s_dm1' % obj)), name='t_%s_dm1_et_data' % obj)
-    w.function('t_%s_dm1_et_data' % obj).setInterpolationOrder(1)
-    wsptools.SafeWrapHist(w, ['t_pt'],
-                          wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_et_file.Get('data_%s_dm10' % obj)), name='t_%s_dm10_et_data' % obj)
-    w.function('t_%s_dm10_et_data' % obj).setInterpolationOrder(1)
-    wsptools.SafeWrapHist(w, ['t_pt'],
-                          wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_et_file.Get('mc_%s' % obj)), name='t_%s_et_mc' % obj)
-    w.function('t_%s_et_mc' % obj).setInterpolationOrder(1)
+for tautype in ['genuine', 'fake']:
+    for iso in ['NoIso',
+                'VLooseIso',
+                'LooseIso',
+                'MediumIso',
+                'TightIso',
+                'VTightIso',
+                'VVTightIso']:
+        for region in ['barrel', 'endcap']:
+            label = '%s_%s_%s' % (tautype, region, iso)
+
+            wsptools.SafeWrapHist(w, ['t_pt'], wsptools.TGraphAsymmErrorsToTH1DForTaus(
+                tau_et_file.Get('data_%s_dm0' % label)), name='t_%s_dm0_et_data' % label)
+            wsptools.SafeWrapHist(w, ['t_pt'], wsptools.TGraphAsymmErrorsToTH1DForTaus(
+                tau_et_file.Get('data_%s_dm1' % label)), name='t_%s_dm1_et_data' % label)
+            wsptools.SafeWrapHist(w, ['t_pt'], wsptools.TGraphAsymmErrorsToTH1DForTaus(
+                tau_et_file.Get('data_%s_dm10' % label)), name='t_%s_dm10_et_data' % label)
+
+            wsptools.MakeBinnedCategoryFuncMap(w, 't_dm', [-0.5, 0.5, 9.5, 10.5],
+                                               't_%s_et_data' % label, ['t_%s_dm0_et_data' % label, 't_%s_dm1_et_data' % label, 't_%s_dm10_et_data' % label])
+
+            wsptools.SafeWrapHist(w, ['t_pt'], wsptools.TGraphAsymmErrorsToTH1DForTaus(
+                tau_et_file.Get('mc_%s' % label)), name='t_%s_et_mc' % label)
+
+            w.function('t_%s_dm0_et_data' % label).setInterpolationOrder(interpOrder)
+            w.function('t_%s_dm1_et_data' % label).setInterpolationOrder(interpOrder)
+            w.function('t_%s_dm10_et_data' % label).setInterpolationOrder(interpOrder)
+            w.function('t_%s_et_mc' % label).setInterpolationOrder(interpOrder)
+
+            w.factory('expr::t_%s_et_ratio("@0/@1", t_%s_et_data, t_%s_et_mc)' % (label, label, label))
+
+        fn = w.factory('expr::t_%s_%s_et_ratio("TMath::Abs(@0) < 1.5 ? @1 : @2", t_eta[0], t_%s_barrel_%s_et_ratio, t_%s_endcap_%s_et_ratio)' %
+            (tautype, iso, tautype, iso, tautype, iso))
+
+
+    # wsptools.SafeWrapHist(w, ['t_pt'],
+    #                       wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_et_file.Get('data_%s_dm0' % obj)), name='t_%s_dm0_et_data' % obj)
+    # w.function('t_%s_dm0_et_data' % obj).setInterpolationOrder(1)
+    # wsptools.SafeWrapHist(w, ['t_pt'],
+    #                       wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_et_file.Get('data_%s_dm1' % obj)), name='t_%s_dm1_et_data' % obj)
+    # w.function('t_%s_dm1_et_data' % obj).setInterpolationOrder(1)
+    # wsptools.SafeWrapHist(w, ['t_pt'],
+    #                       wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_et_file.Get('data_%s_dm10' % obj)), name='t_%s_dm10_et_data' % obj)
+    # w.function('t_%s_dm10_et_data' % obj).setInterpolationOrder(1)
+    # wsptools.SafeWrapHist(w, ['t_pt'],
+    #                       wsptools.TGraphAsymmErrorsToTH1DForTaus(tau_et_file.Get('mc_%s' % obj)), name='t_%s_et_mc' % obj)
+    # w.function('t_%s_et_mc' % obj).setInterpolationOrder(1)
 tau_et_file.Close()
 
 
 w.importClassCode('CrystalBallEfficiency')
 
 w.Print()
+
+taus = [
+    (50.0303226283    , -0.690436537219, 10),
+    (59.5032264387    , -1.95157183364 ,  0),
+    (43.1410575009    ,  0.801236923794,  1),
+    (35.2404971103    ,  1.89180734956 ,  0),
+    (45.5487299326    , -1.33507357888 ,  1),
+    (49.3098431225    , -0.55660978639 ,  1),
+    (24.3615004894    ,  0.786721853764,  1),
+    (59.8846279899    , -0.132008199188,  0),
+    (30.2513219915    , -1.56642180495 , 10),
+    (31.4383629152    ,  1.61171893841 ,  1),
+    (400.4383629152   ,  1.61171893841 ,  1),
+    (500.4383629152   ,  1.61171893841 ,  1),
+    (10000.4383629152 ,  1.61171893841 ,  1),
+]
+
+test_func = w.function("t_genuine_VLooseIso_et_ratio").functor(ROOT.RooArgList(w.argSet("t_pt,t_eta,t_dm")))
+# In the event loop need to pass an array of the argument:
+
+for tau in taus:
+    print test_func.eval(array('d', [tau[0], tau[1], tau[2]]))
+
 w.writeToFile('htt_scalefactors_sm_moriond_v1.root')
 w.Delete()
