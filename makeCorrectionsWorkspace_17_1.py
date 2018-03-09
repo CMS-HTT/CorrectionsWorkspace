b2cbb696c1afdba0824c179d64dfcb5ccb5d1a37
Artur Akhmetshin <artur.akhmetshin@cern.ch>
2018-03-09 20:07:02 +0100

    makeCorrectionsWorkspace_17_1.py: update script to retrieve lepton trigger efficiencies for Run2017 Data & MC

--- makeCorrectionsWorkspace_17_1.py
+++ makeCorrectionsWorkspace_17_1.py
@@ -21,21 +21,38 @@ ROOT.gROOT.LoadMacro("CrystalBallEfficiency.cxx+")
 
 w = ROOT.RooWorkspace('w')
 
-### DESY electron tag and probe results
+### DESY electron & muon tag and probe results
 loc = 'inputs/LeptonEfficiencies'
-loc2 = 'inputs/DESYsyncSF'
 
+# electron triggers
 desyHistsToWrap = [
     (loc+'/Electron/Run2017/Electron_EleTau_Ele24.root',           'MC', 'e_trg_EleTau_Ele24Leg_desy_mc'),
-    (loc+'/Electron/Run2017/Electron_EleTau_Ele24.root',           'Data', 'e_trg_EleTau_Ele24Leg_desy_data')
+    (loc+'/Electron/Run2017/Electron_EleTau_Ele24.root',           'Data', 'e_trg_EleTau_Ele24Leg_desy_data'),
+    (loc+'/Electron/Run2017/Electron_Ele32orEle35.root',           'MC', 'e_trg_SingleEle_Ele32OREle35_desy_mc'),
+    (loc+'/Electron/Run2017/Electron_Ele32orEle35.root',           'Data', 'e_trg_SingleEle_Ele32OREle35_desy_data')
 ]
 
 for task in desyHistsToWrap:
     wsptools.SafeWrapHist(w, ['e_pt', 'expr::e_abs_eta("TMath::Abs(@0)",e_eta[0])'],
                           wsptools.ProcessDESYLeptonSFs(task[0], task[1], task[2]), name=task[2])
 
-for t in ['trg_EleTau_Ele24Leg_desy']:
+for t in ['trg_EleTau_Ele24Leg_desy','trg_SingleEle_Ele32OREle35_desy']:
     w.factory('expr::e_%s_ratio("@0/@1", e_%s_data, e_%s_mc)' % (t, t, t))
+
+# muon triggers
+desyHistsToWrap = [
+    (loc+'/Muon/Run2017/Muon_MuTau_IsoMu20.root',           'MC', 'm_trg_MuTau_Mu20Leg_desy_mc'),
+    (loc+'/Muon/Run2017/Muon_MuTau_IsoMu20.root',           'Data', 'm_trg_MuTau_Mu20Leg_desy_data'),
+    (loc+'/Muon/Run2017/Muon_IsoMu24orIsoMu27.root',           'MC', 'm_trg_SingleMu_Mu24ORMu27_desy_mc'),
+    (loc+'/Muon/Run2017/Muon_IsoMu24orIsoMu27.root',           'Data', 'm_trg_SingleMu_Mu24ORMu27_desy_data')
+]
+
+for task in desyHistsToWrap:
+    wsptools.SafeWrapHist(w, ['m_pt', 'expr::m_abs_eta("TMath::Abs(@0)",m_eta[0])'],
+                          wsptools.ProcessDESYLeptonSFs(task[0], task[1], task[2]), name=task[2])
+
+for t in ['trg_MuTau_Mu20Leg_desy','trg_SingleMu_Mu24ORMu27_desy']:
+    w.factory('expr::m_%s_ratio("@0/@1", m_%s_data, m_%s_mc)' % (t, t, t))
 
 w.importClassCode('CrystalBallEfficiency')
 
